95994eb538c80bfa41627610eb94643826f274d8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2016-06-08 09:14:24 -0700

    Test switch to openjdk 8.

--- site-cookbooks/main/recipes/default.rb
+++ site-cookbooks/main/recipes/default.rb
@@ -9,21 +9,25 @@
 
 include_recipe 'apt-upgrade-once::default'
 
-apt_repository 'webupd8team-java' do
-  uri          'ppa:webupd8team/java'
-  distribution 'xenial'
-end
+#apt_repository 'webupd8team-java' do
+#  uri          'ppa:webupd8team/java'
+#  distribution 'xenial'
+#end
 
-bash 'accept_java_license' do
-  code <<-EOH
-  echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  debconf-set-selections
-  echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-  debconf-set-selections
-  EOH
-end
+#bash 'accept_java_license' do
+#  code <<-EOH
+#  echo debconf shared/accepted-oracle-license-v1-1 select true | \
+#  debconf-set-selections
+#  echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+#  debconf-set-selections
+#  EOH
+#end
 
-package 'oracle-java8-installer' do
+#package 'oracle-java8-installer' do
+#  action :upgrade
+#end
+
+package 'openjdk-8-jdk' do
   action :upgrade
 end
 
