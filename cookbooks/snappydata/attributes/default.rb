c9c123c5d4cf745e4ce91d551bf23be71644a525
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2016-06-09 06:41:12 -0700

    Rename hostname.

--- cookbooks/snappydata/attributes/default.rb
+++ cookbooks/snappydata/attributes/default.rb
@@ -1,7 +1,7 @@
 default["snappydata"]["user"] = 'snappydata'
 default["snappydata"]["group"] = 'snappydata'
-default["snappydata"]["fqdn"] = 'ubuntu-xenial.192.168.55.4.xip.io'
-default["snappydata"]["hostname"] = 'ubuntu-xenial'
+default["snappydata"]["fqdn"] = 'snappydata.192.168.55.4.xip.io'
+default["snappydata"]["hostname"] = 'snappydata'
 default["snappydata"]["ip"] = '192.168.55.4'
 default["snappydata"]["home_dir"] = '/opt/' + default["snappydata"]["user"]
 default["snappydata"]["dir"] = '/opt/' + default["snappydata"]["user"] + '/src'
