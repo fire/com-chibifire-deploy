86bf3ad26c6ac35ec4ae03e50634d9766da18946
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2016-07-09 16:59:32 -0700

    Sets up snappydata and ycsb tests.

--- default-attrib.rb
+++
@@ -1,4 +0,0 @@
-default["snappydata-ycsb"]["user"] = 'snappydata-ycsb'
-default["snappydata-ycsb"]["group"] = 'snappydata-ycsb'
-default["snappydata-ycsb"]["home_dir"] =  '/opt/' + default["snappydata-ycsb"]["user"]
-default["snappydata-ycsb"]["dir"] = '/opt/' + default["snappydata-ycsb"]["user"] + '/src'
--- default-snappydata.rb
+++
@@ -1,7 +0,0 @@
-default["snappydata"]["user"] = 'snappydata'
-default["snappydata"]["group"] = 'snappydata'
-default["snappydata"]["fqdn"] = 'snappydata.192.168.55.4.xip.io'
-default["snappydata"]["hostname"] = 'snappydata'
-default["snappydata"]["ip"] = '192.168.55.4'
-default["snappydata"]["home_dir"] = '/opt/' + default["snappydata"]["user"]
-default["snappydata"]["dir"] = '/opt/' + default["snappydata"]["user"] + '/src'
