23e30275bbba8e563e1707a756af1a36be6c5512
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-01-09 22:02:55 -0800

    Set proper scaling replicas.

--- snappydata/k8s/high-availability/scale.bat
+++ snappydata/k8s/high-availability/scale.bat
@@ -1,4 +1,4 @@
-kubectl scale statefulsets snappydata-server --replicas=5
-kubectl scale statefulsets snappydata-leader --replicas=3
+kubectl scale statefulsets snappydata-server --replicas=3
+kubectl scale statefulsets snappydata-leader --replicas=2
 kubectl scale statefulsets snappydata-locator --replicas=2
 
