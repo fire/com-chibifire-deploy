a35bd1acbd2a75092db81d49cd4bbaa63958cb4c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-02-20 18:25:02 -0800

    Revert "Use only one leader on recommendation by SnappyData."
    This reverts commit cebce35ab79f36a65ad3caccfcb9dd3ef0642d70.

--- snappydata/k8s/high-availability/scale.bat
+++ snappydata/k8s/high-availability/scale.bat
@@ -1,4 +1,4 @@
 kubectl scale statefulsets snappydata-server --replicas=3
-kubectl scale statefulsets snappydata-leader --replicas=1
+kubectl scale statefulsets snappydata-leader --replicas=2
 kubectl scale statefulsets snappydata-locator --replicas=2
 
